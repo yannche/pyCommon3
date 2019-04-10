2f9a6e92f02adfb535530c4a2f048b034d8db1c3
yannche <yann_che2@yahoo.fr>
2019-04-10 20:33:37 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyCommon3',
+      version='0.1',
+      description='nothin',
+      url='https://github.com/yannche/pyCommon3',
+      author='yann',
+      author_email='yann.chevaleyre@lamsade.dauphine.fr',
+      license='MIT',
+      packages=['pyCommon3'],
+      zip_safe=False)
